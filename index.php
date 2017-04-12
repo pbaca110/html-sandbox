5a8a64a93e3458a08d2eb72b6016610f89a74787
Paul Baca <pbaca110@cnm.edu>
2017-04-12 08:51:17 -0600

    added image

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 		<meta charset="utf-8" />
 
 <!--		custom css -->
-
-		<>
+<!--		custom java-->
 		<script type="text/javascript" src="js/scripts.js"></script>
 
 		<link rel="stylesheet" href="style.css" type="text/css"/>
@@ -13,6 +12,15 @@
 		<title> HTML SANDBOX </title>
 	</head>
 	<body>
+		<header>
+			<h1>yoyooyoyo</h1>
+		</header>
 
+		<main>
+			<h2>lullllls</h2>
+		<p>oearnoinaeoirbnaoeinrbonaeobnraenroineoirnoaenrb
+		eaoinrbonerboiaenrobinaerbn;aeonrba</p>
+		</main>
+<img src="img/lul">
 	</body>
 </html>
